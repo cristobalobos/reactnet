82485ddb62b7b1c6f037bada0d3b03d66628602b
cristobalobos <cristobal.lobost@gmail.com>
2019-11-03 22:48:08 -0300

    Hola mundo react

---
+++ app.js
@@ -0,0 +1,9 @@
+﻿class App extends React.Component {
+    //devuelve una descripcion de lo que queremos ver en pantalla
+    //esto ya es jsx combinacion entre js y xml
+    render() {
+        return <h1>Hello React World!</h1>
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
